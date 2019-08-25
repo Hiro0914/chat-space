d994c7c4ce45ca8a4603e6342800809385c5b35e
hiro0914 <hirokura.magic@gmail.com>
2019-08-25 15:29:55 +0900

    incremental search

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,15 +7,21 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @group.users << current_user
+    @users = []
   end
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
+  end
+
+  def edit
+    @users = @group.users.where.not(id: current_user.id)
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  #  
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -9,6 +18,15 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def search
+    # @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    # binding.pry
+    # respond_to do |format|
+    #   format.html
+    #   format.json
+    # end
   end
 
   private
