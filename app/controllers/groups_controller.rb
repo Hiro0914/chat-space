bc40468e8698aefad37ef078ed12e4b2eeed3e84
hiro0914 <hirokura.magic@gmail.com>
2019-09-02 01:34:29 +0900

    make_changes_on_group_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.users << current_user
+    #グループを新規作成するたびに、カレントユーザーが2人分グループに入っていたのでコメントアウト。　@group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
